972d723192a1117110c2f141246e58dcb5207f89
Jonathan do Ouro <j248364@dac.unicamp.br>
2022-10-12 19:38:26 -0300

    first commit

---
+++ src/test/java/com/minhaEmpresaNova/course/CourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minhaEmpresaNova.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
